56e6f40d3de166d814ee669cdaa1efec176eb020
sdegras <sdegrasguemto@yahoo.com>
2023-05-03 01:08:50 +0200

    first commit

--- test_fuzz.rb
+++ test_fuzz.rb
@@ -9,13 +9,16 @@ def fuzz(input_file)
     # Run Radamsa on the current line
     mutated_data, status = Open3.capture2("radamsa", stdin_data: line)
     data = mutated_data
+    count = 0
     if status.success?
       # Do something with the mutated data
+      count = count + 1
       #puts "Mutated data: #{mutated_data.class}"
       #puts "Mutated data: #{mutated_data}"
       data_css=detect_xss_sinks_with_css(data)
       data_xpath=detect_xss_sinks_with_xpath(data)
       #sanitize_input(data)
+      puts "#{count}--itme(s) fuzzed"
       if data_css !="" && data_xpath !=""
         puts "--->Possible XSS detected with css search:#{data_css}"
         puts "--->Possible XSS detected with xpath search:#{data_css}"
