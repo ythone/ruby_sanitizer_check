fbca22f439b193ff97ed5345e6306a145ac48850
sdegras <sdegrasguemto@yahoo.com>
2023-05-02 23:52:27 +0200

    first commit

--- test_fuzz.rb
+++ test_fuzz.rb
@@ -8,11 +8,12 @@ def fuzz(input_file)
   File.foreach(input_file) do |line|
     # Run Radamsa on the current line
     mutated_data, status = Open3.capture2("radamsa", stdin_data: line)
+    data = mutated_data
     if status.success?
       # Do something with the mutated data
       #puts "Mutated data: #{mutated_data.class}"
       puts "Mutated data: #{mutated_data}"
-      data = mutated_data
+      
       sanitize_input(data)
     else
       puts "Error running Radamsa"
